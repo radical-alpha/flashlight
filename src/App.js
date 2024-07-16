826e5997476cfba8c75d27dc51716125f29a670d
radical-alpha <ragavavishal@outlook.com>
2024-07-16 20:42:48 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useTorchLight } from "./useTorch";
 
 function App() {
+  const { isTorchAvailable, on, toggle } = useTorchLight();
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>Is flassh light available : {isTorchAvailable ? "Yes" : "No"}</p>
+      <button onClick={toggle}>{on ? "Off" : "On"}</button>
     </div>
   );
 }
---
+++ src/useTorch.js
@@ -0,0 +1,97 @@
+import React, { useState, useEffect } from "react";
+
+export const useTorchLight = () => {
+  const [permissionState, setPermissionState] = useState("unknown");
+  const [track, setTrack] = useState();
+  const [isTorchAvailable, setIsTorchAvailable] = useState(false);
+  const [on, setOn] = useState(false);
+
+  useEffect(() => {
+    navigator.permissions
+      .query({
+        name: "camera",
+      })
+      .then((permissionStatus) => {
+        console.info("permissionStatus", permissionStatus);
+        let permissionState = permissionStatus.state;
+
+        if (permissionState === "prompt" || permissionState === "unknown") {
+          navigator.mediaDevices
+            .getUserMedia({
+              video: true,
+            })
+            .then(() => {
+              permissionState = "granted";
+            })
+            .catch(() => {
+              permissionState = "denied";
+            });
+        }
+        setPermissionState(permissionState);
+      });
+  }, []);
+
+  useEffect(() => {
+    if (permissionState === "granted") {
+      navigator.mediaDevices.enumerateDevices().then((devices) => {
+        const cameras = devices
+          .filter((device) => device.kind === "videoinput")
+          .reverse();
+        if (cameras.length === 0) {
+          throw "No camera found on this device.";
+        }
+        const camera = cameras[0];
+        console.info("cam device id :: ", camera.deviceId);
+
+        navigator.mediaDevices
+          .getUserMedia({
+            video: {
+              deviceId: {
+                exact: camera.deviceId,
+              },
+            },
+          })
+          .then((stream) => {
+            const track = stream.getVideoTracks().pop();
+            setTrack(track);
+
+            if (track?.getCapabilities().torch) {
+              setIsTorchAvailable(true);
+            } else {
+              setIsTorchAvailable(false);
+            }
+          });
+      });
+    }
+  }, [permissionState]);
+
+  const turnOn = () => {
+    setOn(true);
+
+    track.applyConstraints({
+      advanced: [
+        {
+          torch: true,
+        },
+      ],
+    });
+  };
+
+  const turnOff = () => {
+    setOn(false);
+
+    track.applyConstraints({
+      advanced: [
+        {
+          torch: false,
+        },
+      ],
+    });
+  };
+
+  return {
+    isTorchAvailable,
+    on,
+    toggle: on ? turnOff : turnOn,
+  };
+};
